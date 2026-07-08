853deafe3d7493c6e173ed5e064735e92c139125
makhammatovb <makhammatoffb@gmail.com>
2026-07-08 19:42:13 +0500

    refresh token

--- managers/urls.py
+++ managers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 from .views import (
     RegisterView, 
     UserLoginView, 
@@ -23,6 +25,7 @@ urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'), # checked 
     path('auth/login/', UserLoginView.as_view(), name='login'), # checked
     path('auth/profile/', ProfileView.as_view(), name='profile'), # checked
+    path('auth/refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/change-password/', PasswordChangeView.as_view(), name='change-password'), # checked
     path('managers/', ManagerListView.as_view(), name='manager-list'), # checked
     path('managers/<int:pk>/update/', ManagerUpdateUserView.as_view(), name='manager-update'), # checked
